2ac0dc8329666c69d1f7661b1550bab12a778b12
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-08-06 19:56:08 -0400

    Integración de nuevas pruebas de Tracks, archivos de ci-cd para deploy aws (en caso de)

--- docs/swagger.js
+++ docs/swagger.js
@@ -13,6 +13,9 @@ const swaggerDefinition = {
     {
       url: "http://localhost:3001/api",
     },
+    {
+      url: "https://mighty-reaches-30847.herokuapp.com/api",
+    },
   ],
   components: {
     securitySchemes: {
